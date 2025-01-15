c7b849dd5260494dacd4c5c146ccc14a969c300a
Brielle28 <chukwuemerieclara@gmail.com>
2025-01-15 15:40:59 +0100

    vue config

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="text-red-500">hello from clara....learning vue js</h1>
+  <!-- <div>
+    <div class="w-[50%]">
+      <div>
+        <label for="">name</label>
+        <input type="text" class="w-full " />
+      </div>
+    </div>
+    <div></div>
+  </div> -->
+</template>
